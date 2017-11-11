1f4dea5ead2917ffaa200ab6fa8c7f0efd6b866e
dmitry <pisarev_dmitry@mail.ru>
2017-11-11 15:39:47 +0300

    Fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-$(document).ready(function () {
-    $('.triger-menu').click(function () {
-        $(".fixed-menu").toggleClass("fixed-menu_active")
-    })
+let triggerMenu = document.getElementById("trigger-menu");
+let menu = document.getElementById("fixed-menu");
+let logo = document.getElementById("logo");
+let bars = document.getElementById("hamburger-menu-bars");
+
+console.log(menu);
+ triggerMenu.addEventListener('click',function() {
+   menu.classList.toggle("fixed-menu_active");
+   logo.classList.toggle('logo_fixed');
+   triggerMenu.classList.toggle('close-menu');
+   bars.classList.toggle("close-menu_bars")
 });
