560bbc73e52a093601d42f998084cf6520d39700
dmitry <pisarev_dmitry@mail.ru>
2017-11-13 12:45:08 +0300

    Fixes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
 let triggerMenu = document.getElementById("trigger-menu");
 let menu = document.getElementById("fixed-menu");
-let logo = document.getElementById("logo");
 let bars = document.getElementById("hamburger-menu-bars");
+let teamAccord = $('.team-accordion__item');
 
-console.log(menu);
+
  triggerMenu.addEventListener('click',function() {
-   menu.classList.toggle("fixed-menu_active");
-   logo.classList.toggle('logo_fixed');
-   triggerMenu.classList.toggle('close-menu');
-   bars.classList.toggle("close-menu_bars")
+     if(menu.classList.contains("fixed-menu_active")){
+         menu.classList.remove("fixed-menu_active");
+
+     }else{
+        menu.classList.add("fixed-menu_active");
+     }
+     if(bars.classList.contains("close-menu_bars")){
+         bars.classList.remove("close-menu_bars");
+
+     }else{
+         bars.classList.add("close-menu_bars");
+     }
+
 });
+
+for(let i=0;i<teamAccord.length;i++){
+    teamAccord[i].addEventListener("click",function(){
+        if(!(this.classList.contains('team-accordion__item_active'))) {
+            for(let i =0;i<teamAccord.length;i++ ) {
+               teamAccord[i].classList.remove("team-accordion__item_active");
+            }
+            this.classList.add("team-accordion__item_active")
+        }
+        else{
+            this.classList.remove("team-accordion__item_active")
+        }
+    })
+}
